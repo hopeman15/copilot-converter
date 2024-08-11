4e6f6588ad9e81855638445f2a119e7a22408fee
Kyle Roe <hopeman15@users.noreply.github.com>
2024-08-11 22:55:01 +0200

    [Enhancement] Crytpo Names (#19)
    * crypto name ext
    
    * update finanzfluss converter
    
    ---------
    
    Co-authored-by: hopeman15 <[hopeman15@users.noreply.github.com]>

---
+++ src/main/kotlin/com/hellocuriosity/utils/CryptoExt.kt
@@ -0,0 +1,39 @@
+package com.hellocuriosity.utils
+
+@Suppress("CyclomaticComplexMethod")
+fun String?.toCryptoName(): String? =
+    when (this) {
+        "ALEPH" -> "Aleph Zero"
+        "ALGO" -> "Algorand"
+        "AMP" -> "Amp"
+        "ANKR" -> "Ankr"
+        "ATOM" -> "Cosmos"
+        "BAT" -> "Basic Attention Token"
+        "BCH" -> "Bitcoin Cash"
+        "BTC" -> "Bitcoin"
+        "CGLD" -> "Celo Gold"
+        "COMP" -> "Compound"
+        "DASH" -> "Dash"
+        "EOS" -> "EOS"
+        "ETH" -> "Ethereum"
+        "ETH2" -> "Ethereum 2.0"
+        "EUR" -> "Euro"
+        "FET" -> "Fetch.ai"
+        "GRT" -> "The Graph"
+        "LTC" -> "Litecoin"
+        "MKR" -> "Maker"
+        "NEAR" -> "NEAR Protocol"
+        "OXT" -> "Oraculos"
+        "RLY" -> "Rally"
+        "RONIN" -> "Ronin"
+        "SAND" -> "The Sandbox"
+        "USDC" -> "USD Coin"
+        "VARA" -> "Vara"
+        "VET" -> "VeChain"
+        "XLM" -> "Stellar"
+        "XTZ" -> "Tezos"
+        "ZEC" -> "Zcash"
+        "ZETA" -> "ZetaChain"
+        "ZRX" -> "0x"
+        else -> this
+    }
