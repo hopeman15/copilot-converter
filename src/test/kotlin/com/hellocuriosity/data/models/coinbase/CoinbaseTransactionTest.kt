01ef2a5ace472517f8426fb1ac5fe4afad8c4dd8
Kyle Roe <hopeman15@users.noreply.github.com>
2024-08-11 16:01:14 +0200

    [Testing] Coinbase Transaction Enums (#14)
    * value
    
    * add proper enum testing
    
    ---------
    
    Co-authored-by: hopeman15 <[hopeman15@users.noreply.github.com]>

--- src/test/kotlin/com/hellocuriosity/data/models/coinbase/CoinbaseTransactionTest.kt
+++ src/test/kotlin/com/hellocuriosity/data/models/coinbase/CoinbaseTransactionTest.kt
@@ -1,11 +1,30 @@
 package com.hellocuriosity.data.models.coinbase
 
 import kotlin.test.Test
-import kotlin.test.assertNull
+import kotlin.test.assertEquals
+
+private typealias Testable = Pair<CoinbaseTransaction.TransactionType?, String?>
 
 class CoinbaseTransactionTest {
+    private val data: List<Testable> =
+        listOf(
+            // Testable(expected, given)
+            Testable(null, null),
+            Testable(CoinbaseTransaction.TransactionType.BUY, "Buy"),
+            Testable(CoinbaseTransaction.TransactionType.CONVERT, "Convert"),
+            Testable(CoinbaseTransaction.TransactionType.DEPOSIT, "Deposit"),
+            Testable(CoinbaseTransaction.TransactionType.INFLATION_REWARD, "Inflation Reward"),
+            Testable(CoinbaseTransaction.TransactionType.LEARNING_REWARD, "Learning Reward"),
+            Testable(CoinbaseTransaction.TransactionType.RECEIVE, "Receive"),
+            Testable(CoinbaseTransaction.TransactionType.SELL, "Sell"),
+            Testable(CoinbaseTransaction.TransactionType.SEND, "Send"),
+            Testable(CoinbaseTransaction.TransactionType.STAKING, "Staking Income"),
+        )
+
     @Test
-    fun `TransactionType should result in null when provided with null value`() {
-        assertNull(null.toCoinbaseTransactionType())
+    fun `TransactionType should resolve values`() {
+        data.forEach { (expected, given) ->
+            assertEquals(expected, given.toCoinbaseTransactionType())
+        }
     }
 }
