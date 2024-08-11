a8cb651cc9f048199e3d2ee6b229a7e5841e5dea
hopeman15 <[hopeman15@users.noreply.github.com]>
2024-08-11 15:34:54 +0200

    finanzfluss transaction and test

---
+++ src/test/kotlin/com/hellocuriosity/data/models/finanzfluss/FinanzflussTransactionTest.kt
@@ -0,0 +1,44 @@
+package com.hellocuriosity.data.models.finanzfluss
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+private typealias TestableTransaction = Pair<FinanzflussTransaction.TransactionType?, String?>
+private typealias TestableType = Pair<FinanzflussTransaction.Type?, String?>
+
+class FinanzflussTransactionTest {
+    private val typeData: List<TestableType> =
+        listOf(
+            // TestableType(expected, given)
+            TestableType(null, null),
+            TestableType(FinanzflussTransaction.Type.BOND, "Anleihe"),
+            TestableType(FinanzflussTransaction.Type.FOREIGN_CURRENCY, "Fremdwährung"),
+            TestableType(FinanzflussTransaction.Type.STOCK, "Aktie"),
+        )
+
+    private val transactionTypeData: List<TestableTransaction> =
+        listOf(
+            // TestableTransaction(expected, given)
+            TestableTransaction(null, null),
+            TestableTransaction(FinanzflussTransaction.TransactionType.BOOKING, "Einbuchung"),
+            TestableTransaction(FinanzflussTransaction.TransactionType.BUY, "Kauf"),
+            TestableTransaction(FinanzflussTransaction.TransactionType.COUPON, "Kupon"),
+            TestableTransaction(FinanzflussTransaction.TransactionType.DERECOGNITION, "Ausbuchung"),
+            TestableTransaction(FinanzflussTransaction.TransactionType.DIVIDEND, "Dividende"),
+            TestableTransaction(FinanzflussTransaction.TransactionType.SELL, "Verkauf"),
+        )
+
+    @Test
+    fun `Type should resolve values`() {
+        typeData.forEach { (expected, given) ->
+            assertEquals(expected, given.toFinanzflussType())
+        }
+    }
+
+    @Test
+    fun `TransactionType should resolve values`() {
+        transactionTypeData.forEach { (expected, given) ->
+            assertEquals(expected, given.toFinanzflussTransaction())
+        }
+    }
+}
